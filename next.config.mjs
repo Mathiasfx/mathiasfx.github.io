b242d4de4931c888eeeec0f86217d890d37393a8
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-01-04 20:05:01 -0300

    [ADD] ADD nextconfig for githubpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Genera archivos HTML estáticos
+  images: {
+    unoptimized: true, // Necesario porque GitHub Pages no soporta imágenes optimizadas dinámicamente
+  },
+  distDir: "out", // Define el directorio de salida
+};
 
 export default nextConfig;
