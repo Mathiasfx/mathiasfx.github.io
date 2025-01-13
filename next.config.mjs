037daf1813777607e89a7b1b4653030e08c5abab
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-01-13 16:09:09 -0300

    [UPDATE] add export script and update GitHub Actions for deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   output: "export", // Genera archivos HTML estáticos
   images: {
     unoptimized: true, // Necesario porque GitHub Pages no soporta imágenes optimizadas dinámicamente
   },
   distDir: "out", // Define el directorio de salida
+  basePath: "/my-nextjs-app", // Usa el nombre del repositorio
+  assetPrefix: "/my-nextjs-app", // Usa el nombre del repositorio
 };
 
 export default nextConfig;
